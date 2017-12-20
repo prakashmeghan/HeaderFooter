024c716a314ab928b59bc94d1b39736acad3dcca
prakash meghani <prakashmeghan@gmail.com>
2017-12-20 18:46:20 +0530

    complete header footer code
    by Prakash Meghani

---
+++ app/src/main/java/com/prakashmeghani/dummy/Util/Constants.java
@@ -0,0 +1,19 @@
+package com.prakashmeghani.dummy.Util;
+
+/**
+ * Created by prakash.meghani on 12/20/2017.
+ */
+
+public class Constants {
+    public static final int APP_MENU_FRAG_ID = 1;
+    public static final int PROFILE_FRAG_ID = 2;
+    public static final int GIVE_FRAG_ID = 3;
+    public static final int MYWB_FRAG_ID = 4;
+    public static final int HOME_FRAG_ID = 5;
+
+    public static final int LIKE_FAV_FRAG_ID = 6;
+    public static final int IDEAS_FRAG_ID = 7;
+    public static final int SHARE_FRAG_ID = 8;
+    public static final int TODO_FRAG_ID = 9;
+    public static final int INSPIRE_FRAG_ID = 10;
+}
